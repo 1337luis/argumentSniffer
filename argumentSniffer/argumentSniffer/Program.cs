4e1f578d5a458e0a49db13942dce07d5d1a5b606
Luis Santos <santos.luis1337@gmail.com>
2018-09-17 17:29:12 +0200

    Initial commit

---
+++ argumentSniffer/argumentSniffer/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace argumentSniffer
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            String arguments = System.AppDomain.CurrentDomain.FriendlyName + " ";
+
+            foreach( String argument in args )
+            {
+                arguments += format( argument ) + " ";
+            }
+            Console.WriteLine( arguments );
+            save( arguments );
+        }
+
+        static String format( String arg )
+        {
+            bool returnCom = false;
+            String result = "";
+
+            foreach( Char c in arg )
+            {
+                if( c == ' ' ) returnCom = true;
+            }
+
+            if( returnCom )
+            {
+                result = "\"" + arg + "\"";
+            }
+            else
+            {
+                result = arg;
+            }
+
+            return result;
+        }
+        static void save( String arg )
+        {
+            System.IO.File.WriteAllText( 
+                "launcher_" + 
+                DateTime.Now.Day + "_" + 
+                DateTime.Now.Hour + 
+                DateTime.Now.Minute + 
+                DateTime.Now.Second +
+                ".bat", "@echo off\r\n" + arg );
+        }
+    }
+}
